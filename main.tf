8a7e41ea2f1e0735d7a81d672a92521d7ead6845
root <root@ip-172-31-31-112.ap-southeast-1.compute.internal>
2024-05-13 05:12:07 +0000

    Add ASG

--- main.tf
+++ main.tf
@@ -2,21 +2,54 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-resource "aws_instance" "terraform" {
-  ami           =  "ami-003c463c8207b4dfa"
+#resource "aws_instance" "terraform" {
+#  ami           = "ami-003c463c8207b4dfa"
+#  instance_type = "t2.micro"
+#  vpc_security_group_ids = [aws_security_group.instance.id]
+#  
+#  user_data = <<-EOF
+#              #!/bin/bash
+#              echo "Hello, World" > index.html
+#              nohup busybox httpd -f -p ${var.server_port} &
+#              EOF
+#
+#  user_data_replace_on_change = true
+#
+#  tags = {
+#    Name = "terraform-example"
+#  }
+#}
+
+resource "aws_launch_configuration" "terraform" {
+  image_id      = "ami-003c463c8207b4dfa"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
-  
+  security_groups = [aws_security_group.instance.id]
+
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
+  # Required when using a launch configuration with an ASG.
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-  user_data_replace_on_change = true
+resource "aws_autoscaling_group" "terraform" {
+  launch_configuration = aws_launch_configuration.terraform.name
+  vpc_zone_identifier  = data.aws_subnets.default.ids
 
-  tags = {
-    Name = "terraform-example"
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
 
@@ -30,13 +63,105 @@ resource "aws_security_group" "instance" {
   }
 }
 
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+  # Allow inbound HTTP requests
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "terraform" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.terraform.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
   default     = 8080
 }
 
-output "public_ip" {
-  value       = aws_instance.terraform.public_ip
-  description = "The public IP address of the web server"
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+#output "public_ip" {
+#  value       = aws_instance.terraform.public_ip
+#  description = "The public IP address of the web server"
+#}
+
+output "alb_dns_name" {
+  value       = aws_lb.terraform.dns_name
+  description = "The domain name of the load balancer"
 }
